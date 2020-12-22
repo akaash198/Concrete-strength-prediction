fdae74dbd2a4ddb41517fba003dc27402836d54b
akaash198 <68701225+akaash198@users.noreply.github.com>
2020-12-22 14:24:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+# In[2]:
+
+
+data=st.file_uploader('File uploader')
+
+
+# In[ ]:
+
+
+
+
